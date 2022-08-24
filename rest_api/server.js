21dc4ab9477adc550ae931a1f9b1f21cc8cde2ff
sarmaasis <sarmaasis@gmail.com>
2022-08-24 20:10:39 +0530

    user model and user controller

---
+++ rest_api/server.js
@@ -0,0 +1,16 @@
+const express = require('express'); 
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.get('/', function(req, res){
+ res.json({"tutorial" : "Build REST API with node.js"});
+});
+
+
+app.listen(3000, function(){ console.log('Node server listening on port 3000');});
